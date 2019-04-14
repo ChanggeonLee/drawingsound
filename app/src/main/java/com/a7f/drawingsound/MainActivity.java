409c214e617c85f294e26271481bcf2efa796a88
LeeChangGeon <changgeon718@gmail.com>
2019-04-14 20:18:57 +0900

    transfer_activity

--- app/src/main/java/com/a7f/drawingsound/MainActivity.java
+++ app/src/main/java/com/a7f/drawingsound/MainActivity.java
@@ -1,13 +1,41 @@
 package com.a7f.drawingsound;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button ButtonLogin;
+    Button ButtonSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButtonLogin = (Button)findViewById(R.id.ButtonLogin);
+        ButtonSignup = (Button)findViewById(R.id.ButtonSignup);
+
+        ButtonLogin.setOnClickListener(LoginClickListener);
+        ButtonSignup.setOnClickListener(SignupClickListener);
+
+
     }
+
+    Button.OnClickListener LoginClickListener = new View.OnClickListener() {
+        public void onClick(View v){
+            Intent intent=new Intent(MainActivity.this,HummingActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    Button.OnClickListener SignupClickListener = new View.OnClickListener() {
+        public void onClick(View v){
+            //할일 적어주세염
+        }
+    };
+
+
 }
