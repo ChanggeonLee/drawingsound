5efc41cc17b7529ea8fb5d792d0e7373c68fba33
LeeChangGeon <changgeon718@gmail.com>
2019-04-15 17:38:49 +0900

    transfer_activity

--- app/src/main/java/com/a7f/drawingsound/SheetActivity.java
+++ app/src/main/java/com/a7f/drawingsound/SheetActivity.java
@@ -1,13 +1,31 @@
 package com.a7f.drawingsound;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SheetActivity extends AppCompatActivity {
+
+    Button ButtonApply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sheet);
+
+        ButtonApply = (Button)findViewById(R.id.ButtonApply);
+        ButtonApply.setOnClickListener(Applyonclick);
+
     }
+
+    Button.OnClickListener Applyonclick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(SheetActivity.this, SheetSettingActivity.class);
+            startActivity(intent);
+        }
+    };
+
 }
