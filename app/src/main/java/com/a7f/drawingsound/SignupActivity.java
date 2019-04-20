7bb007e374695cb85eb11af27bf27c733f41a344
jungyeongseo <pp1312pp@naver.com>
2019-04-20 19:27:12 +0900

    signup UI

--- app/src/main/java/com/a7f/drawingsound/SignupActivity.java
+++ app/src/main/java/com/a7f/drawingsound/SignupActivity.java
@@ -1,13 +1,38 @@
 package com.a7f.drawingsound;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
+    Button btn_save;
+    Button btn_cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        btn_save = findViewById(R.id.btn_save);
+        btn_cancel = findViewById(R.id.btn_cancel);
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
