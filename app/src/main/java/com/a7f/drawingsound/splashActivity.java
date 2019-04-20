07e70896af2b0e68f274157bd064c3767725d4e6
LeeChangGeon <changgeon718@gmail.com>
2019-04-20 21:59:51 +0900

    add splash

---
+++ app/src/main/java/com/a7f/drawingsound/splashActivity.java
@@ -0,0 +1,52 @@
+package com.a7f.drawingsound;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashActivity extends AppCompatActivity {
+
+    Intent intent;
+
+    Thread t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        t = new Thread() {
+            public void run() {
+                try{
+                    sleep(1000);
+                    intent = new Intent(splashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e){
+                    //
+                }
+            }
+        };
+
+        t.start();
+    }
+
+    public void splashScreen (final int x) {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(x);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                intent = new Intent(splashActivity.this, MainActivity.class);
+
+//                startActivity(intent);
+//                finish();
+//
+                startActivity(intent);
+                finish();
+            }
+        }).run();
+    }
+}
