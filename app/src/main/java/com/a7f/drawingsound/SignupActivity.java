22b6f5d686bbeb56b1cb9a82d3bd99346c959ad0
jungyeongseo <pp1312pp@naver.com>
2019-04-20 19:50:16 +0900

    passwd hiding

--- app/src/main/java/com/a7f/drawingsound/SignupActivity.java
+++ app/src/main/java/com/a7f/drawingsound/SignupActivity.java
@@ -3,12 +3,15 @@ package com.a7f.drawingsound;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
     Button btn_save;
     Button btn_cancel;
+    EditText text_passwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,8 @@ public class SignupActivity extends AppCompatActivity {
 
         btn_save = findViewById(R.id.btn_save);
         btn_cancel = findViewById(R.id.btn_cancel);
+        text_passwd = findViewById(R.id.text_passwd);
+        text_passwd.setTransformationMethod(new PasswordTransformationMethod());
 
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
