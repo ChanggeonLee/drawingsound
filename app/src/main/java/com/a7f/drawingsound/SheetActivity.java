d1e34ec1a9cc8e9bbd377d5b91424f827fdf4d67
JaeHwan <jaehwlee@gmail.com>
2019-04-30 23:48:37 +0900

    Create example of sheet list

--- app/src/main/java/com/a7f/drawingsound/SheetActivity.java
+++ app/src/main/java/com/a7f/drawingsound/SheetActivity.java
@@ -1,31 +1,33 @@
 package com.a7f.drawingsound;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class SheetActivity extends AppCompatActivity {
 
-    Button ButtonApply;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sheet);
 
-        ButtonApply = (Button)findViewById(R.id.ButtonApply);
-        ButtonApply.setOnClickListener(Applyonclick);
+//        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+//        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
-
-    Button.OnClickListener Applyonclick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(SheetActivity.this, SheetSettingActivity.class);
-            startActivity(intent);
-        }
-    };
-
 }
--- app/src/main/java/com/a7f/drawingsound/SheetListActivity.java
+++ app/src/main/java/com/a7f/drawingsound/SheetListActivity.java
@@ -2,12 +2,34 @@ package com.a7f.drawingsound;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+
+import com.a7f.drawingsound.adapter.SheetAdapter;
+import com.a7f.drawingsound.data.SampleData;
 
 public class SheetListActivity extends AppCompatActivity {
+
+    private SheetAdapter adapter = new SheetAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sheet_list);
+
+        //recycleView 초기화
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+       // recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayout.HORIZONTAL, false));
+        //new LinearLayoutManager(this, LinearLayout.HORIZONTAL, false);
+       recyclerView.setAdapter(adapter);
+
+
+
+        //recyclerView.setAdapter(adapter);
+        adapter.setItems(new SampleData().getItems());
+
     }
 }
