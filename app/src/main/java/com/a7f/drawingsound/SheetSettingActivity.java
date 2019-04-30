e21b1b647aea2bb69a9f173fd95414bcafb45517
jungyeongseo <pp1312pp@naver.com>
2019-04-30 22:22:28 +0900

    sheet Data setting

--- app/src/main/java/com/a7f/drawingsound/SheetSettingActivity.java
+++ app/src/main/java/com/a7f/drawingsound/SheetSettingActivity.java
@@ -3,21 +3,43 @@ package com.a7f.drawingsound;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SheetSettingActivity extends AppCompatActivity {
 
     Button ButtonPrev;
     Button ButtonSet;
+    EditText EditTextTitle;
+    EditText EditTextComposer;
+
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sheet_setting);
+        settingDB();
+        settingHandler();
 
+    }
+
+    private void settingDB(){
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+    }
+
+    private void settingHandler(){
         ButtonPrev = (Button)findViewById(R.id.ButtonPrev);
         ButtonSet = (Button)findViewById(R.id.ButtonSet);
+        EditTextTitle = (EditText)findViewById(R.id.EditTextTitle);
+        EditTextComposer = (EditText)findViewById(R.id.EditTextComposer);
 
         ButtonPrev.setOnClickListener(Prevonclick);
         ButtonSet.setOnClickListener(Setclick);
@@ -35,9 +57,24 @@ public class SheetSettingActivity extends AppCompatActivity {
     Button.OnClickListener Setclick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(SheetSettingActivity.this,SheetListActivity.class);
-            finish();
-            startActivity(intent);
+            String title, composer;
+
+            title = EditTextTitle.getText().toString();
+            composer = EditTextComposer.getText().toString();
+
+            if(!title.isEmpty() && !composer.isEmpty()){
+
+                try {
+                    Sheet sheet = new Sheet(title, composer);
+                    myRef.child("sheets").child(title).setValue(sheet);
+                    myRef.child("sheets").child(composer).setValue(sheet);
+                }catch (Exception e){
+                    //
+                }
+
+                Intent intent = new Intent(SheetSettingActivity.this,SheetListActivity.class);
+                startActivity(intent);
+            }
         }
     };
 }
