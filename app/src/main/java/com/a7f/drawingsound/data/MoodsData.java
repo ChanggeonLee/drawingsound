f8e2477bf7ec90352e006fe2c0d6dfc274015084
LeeChangGeon <changgeon718@gmail.com>
2019-05-20 22:44:07 +0900

    set Mood img

--- app/src/main/java/com/a7f/drawingsound/data/MoodsData.java
+++ app/src/main/java/com/a7f/drawingsound/data/MoodsData.java
@@ -11,13 +11,13 @@ public class MoodsData {
 
     public ArrayList<Mood> getItems() {
 
-        Mood mood1 = new Mood("https://i.ytimg.com/vi/5-mWvUR7_P0/maxresdefault.jpg",
+        Mood mood1 = new Mood("https://firebasestorage.googleapis.com/v0/b/drawingsound-1d381.appspot.com/o/brightness.jpeg?alt=media&token=39180ee2-0073-4dc7-840e-f635bafb92b3",
                 "밝음");
 
-        Mood mood2 = new Mood("https://t1.daumcdn.net/cfile/tistory/0138F14A517F77713A",
+        Mood mood2 = new Mood("https://firebasestorage.googleapis.com/v0/b/drawingsound-1d381.appspot.com/o/calmness.png?alt=media&token=c76726b8-81c2-42e4-8752-194cb18f770a",
                 "잔잔함");
 
-        Mood mood3 = new Mood("https://i.ytimg.com/vi/5-mWvUR7_P0/maxresdefault.jpg",
+        Mood mood3 = new Mood("https://firebasestorage.googleapis.com/v0/b/drawingsound-1d381.appspot.com/o/darkness.png?alt=media&token=615c451d-92c3-4de6-be61-cc5340e53a10",
                 "어두움");
 
         items.add(mood1);
