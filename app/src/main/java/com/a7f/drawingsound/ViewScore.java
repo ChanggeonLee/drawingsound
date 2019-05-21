bef0ca211b91c6aec28a252061a06489902df96f
leehayeong <lhaayyy@naver.com>
2019-05-21 16:49:43 +0900

    add TitleBar in AllActivity

--- app/src/main/java/com/a7f/drawingsound/ViewScore.java
+++ app/src/main/java/com/a7f/drawingsound/ViewScore.java
@@ -3,6 +3,9 @@ package com.a7f.drawingsound;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,11 +19,13 @@ public class ViewScore extends AppCompatActivity {
     private WebView WebViewScore;
     private WebSettings WebSettinsScore;
     private String uri;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_score);
+        setTitle("");
 
         WebViewScore = (WebView)findViewById(R.id.WebViewScore);
         WebViewScore.setWebViewClient(new WebViewClient());
@@ -30,6 +35,11 @@ public class ViewScore extends AppCompatActivity {
         uri = sheetUri();
 
         WebViewScore.loadUrl(uri);
+
+        Toolbar tb = (Toolbar) findViewById(R.id.app_toolbar);
+        setSupportActionBar(tb);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private String sheetUri(){
@@ -44,5 +54,31 @@ public class ViewScore extends AppCompatActivity {
         uri = "http://drawingsound.com:8888/sheet/msheet?key=" + key + "&uid=" + uid;
 
         return uri;
+    }
+
+
+    private void signOut() {
+        mAuth.signOut();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_action, menu) ;
+
+        return true ;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout :
+                signOut();
+                Intent intent = new Intent(ViewScore.this, SigninActivity.class);
+                startActivity(intent);
+                finish();
+                return true ;
+            default :
+                return super.onOptionsItemSelected(item) ;
+        }
     }
 }
